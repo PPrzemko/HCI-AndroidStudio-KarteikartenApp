67fe45fe232b85d85b4f06d1e500a02493557e22
TheWhiteSheep <thw.leon@gmail.com>
2023-06-06 17:37:52 +0200

    init

---
+++ HCI/app/src/main/java/com/example/hci/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.hci;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        Button b=findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
