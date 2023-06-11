0d1ed325f59d7d16cf144ec1fcabadc858f6cff4
Majorasu <46532169+Majorasu@users.noreply.github.com>
2023-06-11 19:52:09 +0200

    done friendlist

--- HCI/app/src/main/java/com/example/hci/FriendlistActivity.java
+++ HCI/app/src/main/java/com/example/hci/FriendlistActivity.java
@@ -3,6 +3,9 @@ package com.example.hci;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class FriendlistActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class FriendlistActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friendlist);
+
+        TextView textView = findViewById(R.id.textViewFriendlistHeadline);
+        textView.setText("Freundesliste");
+
+        LinearLayout linear =findViewById(R.id.LinearLayoutFriendlist);
+        TextView[] textViews = new TextView[20];
+
+        for(TextView txt : textViews){
+            txt = new TextView(this);
+            txt.setText("Hans Peter");
+            txt.setPadding(0,40,0,40);
+            txt.setGravity(Gravity.CENTER);
+            linear.addView(txt);
+        }
     }
 }
