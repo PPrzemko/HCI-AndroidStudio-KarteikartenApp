7d5f6527feb00c18012a82854f325be44676ff92
TheWhiteSheep <thw.leon@gmail.com>
2023-06-11 20:37:25 +0200

    add navbar func

--- HCI/app/src/main/java/com/example/hci/FriendfeedActivity.java
+++ HCI/app/src/main/java/com/example/hci/FriendfeedActivity.java
@@ -2,8 +2,13 @@ package com.example.hci;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -27,6 +32,55 @@ public class FriendfeedActivity extends AppCompatActivity {
             txt.setGravity(Gravity.CENTER);
             linear.addView(txt);
         }
+        navbar();
+
+
+
+
+
+    }
+    public void navbar(){
+        ImageButton a=findViewById(R.id.btnActivity);
+        ImageButton b=findViewById(R.id.btnFriendslist);
+        ImageButton c=findViewById(R.id.btnStack);
+        ImageButton d=findViewById(R.id.btnProfile);
+        ImageButton e=findViewById(R.id.btnLogout);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), FriendfeedActivity.class);
+                startActivity(i);
+            }
+        });
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), FriendlistActivity.class);
+                startActivity(i);
+            }
+        });
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), YourStacksActivity.class);
+                startActivity(i);
+            }
+        });
+        d.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), profile.class);
+                startActivity(i);
+            }
+        });
+        e.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TO DO ADD LOGOUT FUNCTIONALITY
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
--- HCI/app/src/main/java/com/example/hci/profile.java
+++ HCI/app/src/main/java/com/example/hci/profile.java
@@ -2,7 +2,10 @@ package com.example.hci;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class profile extends AppCompatActivity {
 
@@ -10,5 +13,49 @@ public class profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        navbar();
+    }
+    public void navbar() {
+        ImageButton a = findViewById(R.id.btnActivity);
+        ImageButton b = findViewById(R.id.btnFriendslist);
+        ImageButton c = findViewById(R.id.btnStack);
+        ImageButton d = findViewById(R.id.btnProfile);
+        ImageButton e = findViewById(R.id.btnLogout);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), FriendfeedActivity.class);
+                startActivity(i);
+            }
+        });
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), FriendlistActivity.class);
+                startActivity(i);
+            }
+        });
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), YourStacksActivity.class);
+                startActivity(i);
+            }
+        });
+        d.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), profile.class);
+                startActivity(i);
+            }
+        });
+        e.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TO DO ADD LOGOUT FUNCTIONALITY
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
