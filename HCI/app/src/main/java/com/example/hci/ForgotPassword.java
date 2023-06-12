e6863e76ce2e96d14c7e95f74d33a0ea68361b28
mzerai <milel.zeraimicael@stud.h-da.de>
2023-06-12 23:01:34 +0200

    -Abbruch Funktion

--- HCI/app/src/main/java/com/example/hci/ForgotPassword.java
+++ HCI/app/src/main/java/com/example/hci/ForgotPassword.java
@@ -2,7 +2,11 @@ package com.example.hci;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class ForgotPassword extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class ForgotPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+
+    Button neu = findViewById(R.id.passwortNeu);
+    Button abbruch  = findViewById(R.id.abrechenButton);
+
+    neu.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+        }
+    });
+
+    abbruch.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+        }
+    });
+
+
     }
 }
