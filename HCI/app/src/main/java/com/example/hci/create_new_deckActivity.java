07824319dd5b1b4086091425b36423f415b09621
mzerai <milel.zeraimicael@stud.h-da.de>
2023-06-13 10:12:44 +0200

    -Button Erstellen -> Stapel führt zur Mein Stapel Seite zurück

--- HCI/app/src/main/java/com/example/hci/create_new_deckActivity.java
+++ HCI/app/src/main/java/com/example/hci/create_new_deckActivity.java
@@ -3,6 +3,9 @@ package com.example.hci;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class create_new_deckActivity extends AppCompatActivity {
@@ -12,6 +15,16 @@ public class create_new_deckActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_new_deck);
 
+
+        Button deckErstellen = findViewById(R.id.Erstellen);
+
+        deckErstellen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), YourStacksActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 
