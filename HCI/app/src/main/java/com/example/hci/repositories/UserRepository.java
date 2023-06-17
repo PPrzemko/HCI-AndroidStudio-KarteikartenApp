52c40c5d03894107c0b58152ff61376bcdce1f69
TheWhiteSheep <thw.leon@gmail.com>
2023-06-17 12:26:42 +0200

    rename userlist getter

--- HCI/app/src/main/java/com/example/hci/repositories/UserRepository.java
+++ HCI/app/src/main/java/com/example/hci/repositories/UserRepository.java
@@ -1,17 +1,18 @@
 package com.example.hci.repositories;
 
-import com.example.hci.model.Deck;
 import com.example.hci.model.User;
 
 
 import java.util.*;
 
 public class UserRepository { //TODO Singleton werden
-    public HashMap<UUID, User> getUsersById() {
-        return usersById;
+
+
+    public HashMap<UUID, User> getUsersList() {
+        return usersList;
     }
 
-    private final HashMap<UUID, User> usersById = new HashMap<>();
+    private final HashMap<UUID, User> usersList = new HashMap<>();
 
     private static UserRepository instance;
     public static UserRepository getInstance(){
@@ -22,18 +23,18 @@ public class UserRepository { //TODO Singleton werden
     }
 
     public void save(User deck) {
-        usersById.put(deck.getUserId(), deck);
+        usersList.put(deck.getUserId(), deck);
     }
 
     public Optional<User> findById(UUID id) {
-        return Optional.ofNullable(usersById.get(id));
+        return Optional.ofNullable(usersList.get(id));
     }
 
     public Set<User> findDecksByName(Set<String> searchWords) {
         Set<User> returnSet = new HashSet<>();
         for (String word : searchWords) {
             String wordLowerCase = word.toLowerCase();
-            for (User user : usersById.values()) {
+            for (User user : usersList.values()) {
                 if(user.getUsername().toLowerCase().equals(wordLowerCase)){
                     returnSet.add(user);
                 }
