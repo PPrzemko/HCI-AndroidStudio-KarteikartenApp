4b9e24c855143ee4c14dcd50c0aab50e37619ccd
TheWhiteSheep <thw.leon@gmail.com>
2023-06-19 11:12:43 +0200

    rename button fix

--- HCI/app/src/main/java/com/example/hci/MainActivity.java
+++ HCI/app/src/main/java/com/example/hci/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button h=findViewById(R.id.Login);
+        Button LoginButton=findViewById(R.id.Login);
         try {
 
             //JSON DATEI LESEN
